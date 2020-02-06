9406dd1648dd9f12fde138803077ebfb13a49370
appshore <bmichel@appshore.co.uk>
2020-02-06 15:24:30 +0000

    health lambda

---
+++ src/handlers/health.ts
@@ -0,0 +1,17 @@
+import 'source-map-support/register';
+
+import { crudReturnInter } from './crunInterface';
+
+/**
+ * check health
+ */
+export const check = (): crudReturnInter => ({
+  statusCode: 200,
+  body: JSON.stringify(
+    {
+      message: 'Function Health executed successfully!',
+    },
+    null,
+    2
+  ),
+});
---
+++ test/health.test.ts
@@ -0,0 +1,17 @@
+import { check } from '../src/handlers/health';
+
+describe('Health check', () => {
+  test('the returned object', () => {
+    const response = check();
+
+    expect(response).toHaveProperty('statusCode');
+    expect(response.statusCode).toEqual(200);
+
+    expect(response).toHaveProperty('body');
+    expect(typeof response.body).toBe('string');
+
+    const body = JSON.parse(response.body);
+    expect(body).toHaveProperty('message');
+    expect(body.message).toBe('Function Health executed successfully!');
+  });
+});
